3d26a761fdc5f9a6f082cab80278a0bad842b4ca
CBurton <burton.chris13@gmail.com>
2024-04-14 15:47:53 -0500

    update count down and stylings

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 //Set the date we're counting down to
-var countDownDate = new Date("May 10 2024 18:59:59").getTime();
+var countDownDate = new Date("May 17 2024 18:59:59").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
